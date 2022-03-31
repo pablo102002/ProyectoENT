53b3a536f34b4b0c4511fbc013df6ca11fd840c0
johann <johann.teran2000@gmail.com>
2022-03-31 16:14:53 +0200

    Johann Signed-off-by: johann <johann.teran2000@gmail.com>

--- Prueba/src/p1/prueba.java
+++ Prueba/src/p1/prueba.java
@@ -5,6 +5,7 @@ public class prueba {
 	public static void main(String[] args) {
 		System.out.println("Prueba1");
 		System.out.println("Prueba2");
-	}
+		System.out.println("Prueba Johann");
+		}
 
 }
